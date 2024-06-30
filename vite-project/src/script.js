40d71b8b7c31904cbac1a871742a878440232057
1shChheda <1shvenom786@gmail.com>
2024-06-30 19:31:43 +0530

    implemented vite bundler and object transformations

--- vite-project/src/script.js
+++ vite-project/src/script.js
@@ -1,31 +1,43 @@
-console.log("Hello!");
-console.log(THREE);
+import './style.css'
+import * as THREE from 'three';
 
 /* Scene */
 const scene = new THREE.Scene();
 
 /* Object (Red Cube) */
-const geometry = new THREE.BoxGeometry(1, 1, 1); // arg: height, width, depth
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+    // alongside use of Scene Graph
 
-/* TRANSFORM OBJECT */
-    // POSITION
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-    // INSTEAD OF MANUALLY EDITING INDIVIDUAL positions, we can use:
-mesh.position.set(0.7, -0.6, -0.2);
+    // use of "Group"
+const group = new THREE.Group();
+scene.add(group);
 
-    // SCALE
-mesh.scale.set(2,1,1);
-// mesh.scale.setScalar (3); // for Uniform Scaling
+group.position.y = 1;
+group.scale.z = 2;
+group.rotation.x = Math.PI / 4;
 
-    // ROTATION
-mesh.rotation.reorder('YXZ'); // to change the order that the rotations are applied
-mesh.rotation.x = Math.PI / 4;
-mesh.rotation.y = Math.PI / 4;
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.7, 0.7, 0.7),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+cube2.position.set(2, 0, 0);
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.7, 0.7, 0.7),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cube3.position.set(-2, 0, 0);
+group.add(cube3);
+
 
 /* AxesHelper */
 const axesHelper = new THREE.AxesHelper();
@@ -51,7 +63,7 @@ camera.position.z = 3; // since the dim. of cube is 1,1,1 -> position value is b
 scene.add(camera);
 
 // camera.lookAt(new THREE.Vector3(3,0,0));
-camera.lookAt(mesh.position); 
+// camera.lookAt(mesh.position); 
 
 /* Renderer */
 
