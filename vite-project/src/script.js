92671ccaf74a6fa3c7d6dc305699798d0345a374
1shChheda <1shvenom786@gmail.com>
2024-07-03 12:37:07 +0530

    explored custom camera controls & control addons

--- vite-project/src/script.js
+++ vite-project/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
 import { gsap } from 'gsap';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 /* Scene */
 const scene = new THREE.Scene();
@@ -21,6 +22,19 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 };
+
+/* Mouse Coordinates */
+
+    // to store coordinates
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5; 
+    cursor.y = - (event.clientY / sizes.height - 0.5);
+    console.log(cursor);
+});
 
 /* Camera */
     // using Perspective Camera 
@@ -49,25 +63,58 @@ const renderer = new THREE.WebGLRenderer({
     // use "setSize()" method to update the size of the renderer
 renderer.setSize(sizes.width, sizes.height);
 
+/* Additional Controls */
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // smooth controls
+
 /* To estimate Frame rate */
 let clock = new THREE.Clock();
 
 /* Create tween using "gsap" */
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
-gsap.to(mesh.position, { duration: 1, delay: 3, x: 0 });
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+// gsap.to(mesh.position, { duration: 1, delay: 3, x: 0 });
+
+/* Creating timeline for Gsap animations */
+// gsap.timeline()
+//     .to(mesh.position, { duration: 2, delay: 2, y: 2 })
+//     .to(mesh.position, { duration: 3, delay: 3, z: 0.5, ease: 'bounce' })
+
+// extra: to resize the window so that we don't have to reload again and again for new render when we resize the screen
+window.addEventListener('resize', () => {
+
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // update the camera too
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+
+    
+});
 
 function animate() {
-    window.requestAnimationFrame(animate);
-
     // elapsedTime: total time that has passed since the clock was started
         // continues to increase as long as the clock is running.
     const elapsedTime = clock.getElapsedTime();
     // console.log(elapsedTime);
 
     mesh.rotation.z = Math.cos(elapsedTime);
+    
+    /* for normal camera position controls */
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+    // camera.position.y = cursor.y * 3;
+    // camera.lookAt(mesh.position);
+
+    controls.update(); // Update controls
+
 
     // NOW, RENDER!
     renderer.render(scene, camera); // arg: scene, camera
+    window.requestAnimationFrame(animate);
+
 }
 
 animate();
